9da4891a0c577f0e2f5754667d45a4f4c28e3c49
Ragil14 <rahil.gilani@gmail.com>
2022-04-26 14:22:56 -0500

    add resumeData

---
+++ src/resumeData.js
@@ -0,0 +1,181 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/ragil1114",
+    "name": "Rahil Gilani",
+    "role": "Full-Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/rahil-gilani-2a463684/",
+    "zoomid": "473 050 0442",
+    "roleDescription": " I like exploring various cutting-edge front & back-end technologies, read books, watch/play sports, or simply play video games in my free time. If I'm not doing any of these things, you'll find me hanging out with my family & friends.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahil-gilani-2a463684/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ragil1114",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/Rags2Riches01",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am in my final stretch, currently attending the Tech Program at SMU. Pursuing a Full-Stack Web Development Certificate. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Dallas, TX",
+    "website":"https://ragil1114.github.io/portfolio/",
+    "education":[
+      {
+        "UniversityName":"Southern Methodist University",
+        "specialization":"Full-Stack Web Development",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+        "Achievements":"Certificate"
+      },
+      {
+        "UniversityName":"Brookhaven College",
+        "specialization":"Science",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2013",
+        "Achievements":"Associates Degree"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Breign Infotech",
+        "specialization":"Jr Software Engineer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2022",
+        "Achievements":"Masssive experience in developing web applications using React, NodeJS, Express, MySQL, SQL, HTML, CSS, Bootstrap, JavaScript, jQuery, REST, PWA and many more."
+      },
+      {
+        "CompanyName":"Cubic Technologies",
+        "specialization":"Tech Recruiter",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2021",
+        "Achievements":"Placed countless developers & business professionals in various fortune 500 companies."
+      }
+    ],
+    "skillsDescription":"Qualifications",
+    "skills":[
+      {
+        "skillname":"HTML"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"jQuery"
+      },
+      {
+        "skillname":"Bootstrap"
+      },
+      {
+        "skillname":"React"
+      },
+      {
+        "skillname":"Node"
+      },
+      {
+        "skillname":"Express"
+      },
+      {
+        "skillname":"NoSQL"
+      },
+      {
+        "skillname":"Sequelize"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"MySQL"
+      },
+      {
+        "skillname":"RESTful API"
+      },
+      {
+        "skillname":"GraphQL"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Mongoose"
+      },
+      {
+        "skillname":"Git"
+      },
+      {
+        "skillname":"GitHub"
+      },
+      {
+        "skillname":"Heroku"
+      },
+      {
+        "skillname":"Jest"
+      },
+      {
+        "skillname":"PWA"
+      },
+      {
+        "skillname":"OOP"
+      },
+      {
+        "skillname":"TDD"
+      },
+      {
+        "skillname":"ORM"
+      },
+      {
+        "skillname":"MVC"
+      },
+      {
+        "skillname":"Agile / Scrum / Waterfall"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"E-Comm Site",
+        "description":"Back-end CMS app",
+        "imgurl":"images/portfolio/retrocam.jpg",
+        "url":"https://github.com/ragil1114/E-commSite.git"
+      },
+      {
+        "name":"Social Media App",
+        "description":"Full-Stack Web App",
+        "imgurl":"images/portfolio/into-the-light.jpg",
+        "url":"https://chop-it-up.herokuapp.com/"
+      },
+      {
+        "name":"Weather App",
+        "description":"Mobile App",  
+        "imgurl":"images/portfolio/weather.jpg",
+        "url":"https://ragil1114.github.io/weather-dashboard/"
+      },
+      {
+        "name":"Portfolio Generator",
+        "description":"CLI App",
+        "imgurl":"images/portfolio/origami.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"They said, don't give up on your dreams. So I went back to sleep.",
+        "name":"World's smartest person"
+      },
+      {
+        "description":"All my life I thought air was free... Until I bought a bag of chips.",
+        "name":"IYKYK"
+      }
+    ]
+}
+  
+export default resumeData

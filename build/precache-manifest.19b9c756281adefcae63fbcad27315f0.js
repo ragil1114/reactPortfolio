817dd1327bee8fa6aad5ef3f80fea484f6f7395a
Ragil14 <rahil.gilani@gmail.com>
2022-06-21 23:12:09 -0500

    update bio

---
+++ build/precache-manifest.19b9c756281adefcae63fbcad27315f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f942eaf916d7d12a376",
+    "url": "/reactPortfolio/static/css/main.41a417f7.chunk.css"
+  },
+  {
+    "revision": "9f942eaf916d7d12a376",
+    "url": "/reactPortfolio/static/js/main.9f942eaf.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/reactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "182ec8ef693a7f08e0dfd7738a0a4165",
+    "url": "/reactPortfolio/index.html"
+  }
+];
